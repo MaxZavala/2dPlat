3de0737d3803046768e11ea9c42471691757a8b6
maxwell <maxwell.s.zavala@gmail.com>
2023-11-05 20:47:43 -0800

    added update visuals on update

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //stores local transform
+    public Transform playerTransform;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("hello world");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        updateVisuals();
+
+    }
+    public void updateVisuals()
+    {
+
     }
 }
