1c284403b0dfc8a9642006a27e02d1e8871519fb
maxwell <maxwell.s.zavala@gmail.com>
2023-11-05 21:46:41 -0800

    added floor to main scene. implemented input handeling for player left and right keys.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,11 +7,15 @@ public class Player : MonoBehaviour
     //stores local transform
     public Transform playerTransform;
 
+    public Rigidbody2D playerRidgedBody;
+
     // Start is called before the first frame update
     void Start()
     {
         
+        
     }
+   
 
     // Update is called once per frame
     void Update()
@@ -20,8 +24,47 @@ public class Player : MonoBehaviour
         updateVisuals();
 
     }
+
+    public void assign_player_position(float x_direction) {
+
+       
+        playerTransform.position = new Vector3(playerTransform.position.x + x_direction, playerTransform.position.y, playerTransform.position.z);
+
+    }
+
+    
+
     public void updateVisuals()
     {
 
+        
+
+        //get inputs
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            assign_player_position(-1f);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            assign_player_position(1f);
+
+        }
+      
+            
+  
+
     }
 }
+
+
+
+
+
+
+
+            
+
+
+
+
+  
